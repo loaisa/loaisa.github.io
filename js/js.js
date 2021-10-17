08671f20e2ad88e87184652b5330b42d78804c95
loaisa <60156147+loaisa@users.noreply.github.com>
2021-10-17 16:28:07 +0300

    adding my files to my repository

---
+++ js/js.js
@@ -0,0 +1,122 @@
+$(document).ready(function() {
+    $(function() {
+
+        $('#midground').css({ backgroundPosition: '0px 0px' });
+        $('#foreground').css({ backgroundPosition: '0px 0px' });
+        $('#background').css({ backgroundPosition: '0px 0px' });
+
+        $('#midground').animate({
+            backgroundPosition: "(-10000px -2000px)"
+        }, 240000, 'linear');
+
+        $('#foreground').animate({
+            backgroundPosition: "(-10000px -2000px)"
+        }, 120000, 'linear');
+
+        $('#background').animate({
+            backgroundPosition: "(-10000px -2000px)"
+        }, 480000, 'linear');
+
+    });
+    $(window).scroll(function() {
+
+        const headerHeight = $('#header').innerHeight();
+        //высота до блока main, как скролл доходит до этой высоты, срабабывает функц.
+        const infoOffset = $('main').offset().top;
+        const scrolled = $(this).scrollTop();
+        if (scrolled > infoOffset) {
+            //шапка прилипла
+            $('#header_content').addClass('fixed');
+        } else if (scrolled < headerHeight) {
+            //шапка отлипла
+            $('#header_content').removeClass('fixed');
+        }
+
+    });
+    $(window).scroll(function() {
+            //блок crawl
+            const element = $("#crawl");
+            const spaceHeightsize = $('.space').innerHeight()
+            const spaceHeight = $('.space').offset().top - spaceHeightsize
+            const scrolled = $(this).scrollTop()
+
+            if (scrolled > spaceHeight) {
+                $('.crawl').addClass('spaceAnim')
+            }
+        })
+        // $("#nav").on("click", "a", function(event) {
+        //     event.preventDefault();
+        //     let id = $(this).attr('href'),
+        //         top = $(id).offset().top;
+        //     $('body,html').animate({ scrollTop: top }, 1500);
+        // })
+    const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+    if (menuLinks.length > 0) {
+        menuLinks.forEach(menuLink => {
+            menuLink.addEventListener("click", onMenuLinkClick);
+        });
+
+        function onMenuLinkClick(e) {
+            const menuLink = e.target;
+            if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+                const gotoBlock = document.querySelector(menuLink.dataset.goto);
+                const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('.header').offsetHeight;
+                window.scrollTo({
+                    top: gotoBlockValue,
+                    behavior: "smooth"
+                });
+                e.preventDefault();
+            }
+        }
+    }
+    let value = 1344
+    if ($(window).width() > value) {
+        $('.slider').slick({
+            arrows: true,
+            adaptiveHeight: true,
+            slidesToShow: 3,
+            infinite: true,
+            centerMode: true,
+
+        })
+    } else {
+        $('.slider').slick({
+            arrows: true,
+            adaptiveHeight: true,
+            slidesToShow: 1,
+            infinite: true,
+        })
+    }
+
+
+
+})
+SmoothScroll({
+    // Время скролла 400 = 0.4 секунды
+    animationTime: 800,
+    // Размер шага в пикселях 
+    stepSize: 75,
+
+    // Дополнительные настройки:
+
+    // Ускорение 
+    accelerationDelta: 30,
+    // Максимальное ускорение
+    accelerationMax: 2,
+
+    // Поддержка клавиатуры
+    keyboardSupport: true,
+    // Шаг скролла стрелками на клавиатуре в пикселях
+    arrowScroll: 50,
+
+    // Pulse (less tweakable)
+    // ratio of "tail" to "acceleration"
+    pulseAlgorithm: true,
+    pulseScale: 4,
+    pulseNormalize: 1,
+
+
+    // Поддержка тачпада
+    touchpadSupport: true,
+
+})
---
+++ js/popup.js
@@ -0,0 +1,118 @@
+const popupLinks = document.querySelectorAll('.popup-link')
+const Body = document.querySelector('body')
+const lockPadding = document.querySelectorAll('.lock-padding')
+let unlock = true
+const timeout = 800
+
+if (popupLinks.length > 0) {
+    for (let index = 0; index < popupLinks.length; index++) {
+        const popupLink = popupLinks[index]
+        popupLink.addEventListener('click', function (e) {
+            const popupName = popupLink.getAttribute('href').replace('#', '')
+            const curentPopup = document.getElementById(popupName)
+            popupOpen(curentPopup)
+            e.preventDefault()
+        })
+    }
+}
+
+const popupCloseIcon = document.querySelectorAll('.close-popup')
+
+if (popupCloseIcon.length > 0) {
+    for (let index = 0; index < popupCloseIcon.length; index++) {
+        const element = popupCloseIcon[index];
+        element.addEventListener('click', function (e) {
+            popupClose(element.closest('.popup'))
+            e.preventDefault
+        })
+
+    }
+}
+
+function popupOpen(curentPopup) {
+    let vids = document.querySelectorAll('#video-block') //получилл список всех видео, прошёлся циклом. зафиксировал mouover-ом нужный iframe и после закрывания попапа, запаузил нужное видео
+    $.each(vids, function (index, value) {
+        $(value).mouseover(function (e) {
+            currentiframe = e.target
+        })
+    })
+    if (curentPopup && unlock) {
+        const popupActive = document.querySelector('.popun.open')
+        if (popupActive) {
+            popupClose(popupActive, false)
+        }
+        else {
+            BodyLock();
+        }
+        curentPopup.classList.add('open')
+        curentPopup.addEventListener('click', function (e) {
+            if (!e.target.closest('.popup__content')) {
+                popupClose(e.target.closest('.popup'))
+                currentiframe.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*')
+
+            }
+
+
+        })
+    }
+
+    document.querySelector('.slick-arrow').classList.add('hide')
+    document.querySelector('.slick-next').classList.add('hide')
+}
+
+
+
+function popupClose(popupActive, doUnlock = true) {
+
+
+    if (unlock) {
+        popupActive.classList.remove('open')
+        if (doUnlock) {
+            BodyUnLock();
+        }
+
+    }
+    Body.classList.remove('lock')
+
+    document.querySelector('.slick-arrow').classList.remove('hide')
+    document.querySelector('.slick-next').classList.remove('hide')
+
+}
+
+
+
+function BodyLock() {
+    const lockPaddingValue = window.innerWidth - document.querySelector('.video').offsetWidth + 'px'
+    for (let index = 0; index < lockPadding.length; index++) {
+        const element = lockPadding[index];
+        // element.style.paddingRight = lockPaddingValue
+    }
+    // Body.style.paddingRight = lockPaddingValue
+    Body.classList.add('lock')
+
+    unlock = false;
+    setTimeout(function () {
+        unlock = true;
+    }, timeout)
+}
+
+function BodyUnLock() {
+    setTimeout(function () {
+        for (let index = 0; index < lockPadding.length; index++) {
+            const element = lockPadding[index];
+            // element.style.paddingRight = '0px'
+        }
+        // Body.style.paddingRight = '0px'
+    })
+    unlock = false
+    setTimeout(function () {
+        unlock = true
+    }, timeout)
+}
+
+document.addEventListener('keydown', function (e) {
+    if (e.which === 27) {
+        const popupActive = document.querySelector('.popup.open')
+        popupClose(popupActive)
+    }
+})
